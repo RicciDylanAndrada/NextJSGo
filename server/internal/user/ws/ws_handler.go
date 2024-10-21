983695562a3769df17803553ac554c19fb90c5bd
Ricci <riccidandrada@gmail.com>
2024-10-21 01:03:04 -0400

    add joinRoom getRoom and getClient endpoints

--- server/internal/user/ws/ws_handler.go
+++ server/internal/user/ws/ws_handler.go
@@ -46,7 +46,7 @@ c.JSON(http.StatusOK,req)
 var upgrader = websocket.Upgrader{
 	ReadBufferSize: 1024,
 	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http) bool{
+	CheckOrigin: func(r *http.Request) bool{
 		// origin := r.Header.Get("Origin")
 
 		// return origin =="http://localhost:3000"
@@ -57,13 +57,13 @@ func (h *Handler) JoinRoom(c *gin.Context){
 conn,err:=upgrader.Upgrade(c.Writer,c.Request,nil)
 
 if err!=nil{
-	 c.JSON(http.StatusBadRequest,gin.H{"error joining room":err.Error{}})
+	 c.JSON(http.StatusBadRequest,gin.H{"error joining room":err.Error()})
 	 return 
 }
 
-roomID:=c.Param("roomid")
-clientID:=c.Param("userId")
-username:=c.Param("username")
+roomID:=c.Param("roomId")
+clientID:=c.Query("userId")
+username:=c.Query("username")
 cl:=&Client{
 	Conn: conn,
 	Message: make(chan *Message,10),
@@ -72,7 +72,7 @@ cl:=&Client{
 	Username: username,
 }
 m:=&Message{
-	Content:  username+ "new user joined the room",
+	Content:  "A new user has joined the room",
 	RoomID: roomID,
 	Username: username,
 	
@@ -89,7 +89,53 @@ h.hub.Register<-cl
 // send message to broadcast channel
 h.hub.Broadcast<-m
 
-//write message
-//read message
+go cl.writeMessage()
+
+cl.readMessage(h.hub)
+
+}
+type RoomRes struct{
+	ID string `json:"id"`
+	Name string `json:"name"`
+
+}
+
+func (h *Handler) GetRooms(c *gin.Context){
+	rooms:=make([]RoomRes,0)
+
+	for _,r:=range h.hub.Rooms{
+		rooms=append(rooms,RoomRes{
+			ID:r.ID,
+			Name: r.Name,
+		})
+	}
+	c.JSON(http.StatusOK,rooms)
+
+}
+
+
+type ClientRes struct{
+	ID string `json:"id"`
+	Username string `json:"username"`
+
+}
+
+func (h *Handler) GetClients(c *gin.Context){
+	var clients []ClientRes
+	roomId:=c.Param("roomId")
+
+	if _,ok:=h.hub.Rooms[roomId];!ok{
+		// no one in room so add empty clients
+		clients=make([]ClientRes,0)
+		c.JSON(http.StatusOK,clients)
+	}
+	for _,c:=range h.hub.Rooms[roomId].Clients{
+		// there ar eclient so we wanna loop over each client and add them into clients array
+		clients=append(clients, ClientRes{
+			ID:c.ID,
+			Username: c.Username,
+		})
+	}
+	c.JSON(http.StatusOK,clients)
 
 }
